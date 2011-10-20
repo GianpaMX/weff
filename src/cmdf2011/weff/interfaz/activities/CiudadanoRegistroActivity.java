91655bb65493fa10bee1c69f65b0aecddd2a4f8f
Juan Villa <juan64@gmail.com>
2011-10-20 11:06:45 -0500

    traducido al español

--- src/cmdf2011/weff/interfaz/activities/CiudadanoRegistroActivity.java
+++ src/cmdf2011/weff/interfaz/activities/CiudadanoRegistroActivity.java
@@ -1,24 +1,23 @@
-package cmdf2011.weff;
+package cmdf2011.weff.interfaz.activities;
 
-import cmdf2011.weff.interfaz.activities.CiudadanoRegistroActivity;
+import cmdf2011.weff.R;
+import cmdf2011.weff.R.id;
+import cmdf2011.weff.R.layout;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class CiudadanoRegistroActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.ciudadano_registro);
         
-        Button b = (Button) findViewById(R.id.button1);
+        Button b = (Button) findViewById(R.id.botonRegistro);
         b.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), CiudadanoRegistroActivity.class);
-                startActivityForResult(intent, 0);
             }
         });
     }
