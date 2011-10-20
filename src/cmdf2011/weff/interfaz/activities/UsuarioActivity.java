87b06df5d82e1e3390f58b82866d895ea8af7c25
fuy <estudiante.fidel@gmail.com>
2011-10-20 12:25:44 -0500

    Creación de clases ReportaFugaActivities y xml

--- src/cmdf2011/weff/interfaz/activities/UsuarioActivity.java
+++ src/cmdf2011/weff/interfaz/activities/UsuarioActivity.java
@@ -3,10 +3,18 @@ package cmdf2011.weff.interfaz.activities;
 import cmdf2011.weff.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class UsuarioActivity extends Activity{
 
+	TextView tvTitulo;
+	EditText etIdCiudadano, etPassCiudadano;
+	Button bEntrar, bUsuarioNoRegistrado;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
