3781c991a27d63b7e2e58a13f4bbaa3169220a8a
fuy <estudiante.fidel@gmail.com>
2011-10-20 10:44:20 -0500

    Se agregó la clase UsuarioActivity y el usuario.xml

---
+++ src/cmdf2011/weff/interfaz/activities/UsuarioActivity.java
@@ -0,0 +1,19 @@
+package cmdf2011.weff.interfaz.activities;
+
+import cmdf2011.weff.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class UsuarioActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.usuario);
+	}
+	
+	
+
+}
