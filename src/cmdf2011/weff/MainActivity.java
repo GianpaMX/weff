1bdf0ba4cb3ca7c8a4566180590e57afe2dd113d
Juan Villa <juan64@gmail.com>
2011-10-20 03:29:21 -0500

    citizen register form

--- src/cmdf2011/weff/MainActivity.java
+++ src/cmdf2011/weff/MainActivity.java
@@ -1,7 +1,10 @@
 package cmdf2011.weff;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,13 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button b = (Button) findViewById(R.id.button1);
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), CitizenRegisterActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
 }
