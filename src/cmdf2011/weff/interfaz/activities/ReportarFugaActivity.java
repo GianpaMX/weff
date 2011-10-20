2eac6785abb74e55565e63bd59513a96f8f7db6c
fuy <estudiante.fidel@gmail.com>
2011-10-20 12:26:05 -0500

    kjkj

---
+++ src/cmdf2011/weff/interfaz/activities/ReportarFugaActivity.java
@@ -0,0 +1,24 @@
+package cmdf2011.weff.interfaz.activities;
+
+
+import cmdf2011.weff.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class ReportarFugaActivity extends Activity{
+	
+	private Button bReportaFuga, bEstadoReporte;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.reporta_fuga);
+		
+	}
+	
+	
+	
+
+}
