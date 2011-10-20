1c326692c3616bf8c355fc812772ec6435b23da9
fuy <estudiante.fidel@gmail.com>
2011-10-20 09:11:05 -0500

    Clase Citizien (Ciudadano) agregada

---
+++ src/cmdf2011/weff/beans/Citizen.java
@@ -0,0 +1,75 @@
+package cmdf2011.weff.beans;
+
+public class Citizen {
+	
+
+	private String nombre, apellidoP, apellidoM;
+	private String sexo;
+	private String tel, cel;
+	private String calle, numExt, numInt, colonia;
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidoP() {
+		return apellidoP;
+	}
+	public void setApellidoP(String apellidoP) {
+		this.apellidoP = apellidoP;
+	}
+	public String getApellidoM() {
+		return apellidoM;
+	}
+	public void setApellidoM(String apellidoM) {
+		this.apellidoM = apellidoM;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getCel() {
+		return cel;
+	}
+	public void setCel(String cel) {
+		this.cel = cel;
+	}
+	public String getCalle() {
+		return calle;
+	}
+	public void setCalle(String calle) {
+		this.calle = calle;
+	}
+	public String getNumExt() {
+		return numExt;
+	}
+	public void setNumExt(String numExt) {
+		this.numExt = numExt;
+	}
+	public String getNumInt() {
+		return numInt;
+	}
+	public void setNumInt(String numInt) {
+		this.numInt = numInt;
+	}
+	public String getColonia() {
+		return colonia;
+	}
+	public void setColonia(String colonia) {
+		this.colonia = colonia;
+	}
+	
+	
+
+}
