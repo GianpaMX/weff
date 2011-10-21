d691fc83728dd31c79fb1e86e944987195cfffc3
Juan Villa <juan64@gmail.com>
2011-10-20 19:10:57 -0500

    ticket fuga con spinners y autocomplete (falta arreglar layout)

--- src/cmdf2011/weff/interfaz/activities/MenuPrincipal.java
+++ src/cmdf2011/weff/interfaz/activities/MenuPrincipal.java
@@ -1,16 +1,23 @@
 package cmdf2011.weff.interfaz.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuPrincipal extends Activity {
 
 	/** Called when the activity is first created. */
-	   @Override
-	   public void onCreate(Bundle savedInstanceState) {
-	      super.onCreate(savedInstanceState);
-	      setContentView(R.layout.menuprincipal);
-	      
-	      
-	   }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.menuprincipal);
+
+	}
+
+	public void reportarFuga(View v) {
+		Intent i = new Intent(v.getContext(), TicketFugaActivity.class);
+		startActivity(i);
+	}
+
 }
