69bf37aca485c3e8451d748a264dfb33375bd5fe
Vicente Mendoza <vicentemmor@yahoo.com.mx>
2011-10-20 23:27:10 -0500

    tickets

---
+++ src/cmdf2011/weff/beans/Ticket.java
@@ -0,0 +1,125 @@
+package cmdf2011.weff.beans;
+
+public class Ticket {
+
+	String y_calle, asunto, punto_referencia, id_sentido;  
+	String fecha_ingreso, descripcion, id_prioridad,  entre_calle; 
+	String id_tramo,  id_etapa,  id_area,  id_lugar,  latitud, longitud;
+	String id_motivo,  id_tipo,  id_via,  id_ciudadano,  id_ticket;
+	
+	public String getY_calle() {
+		return y_calle;
+	}
+	public void setY_calle(String y_calle) {
+		this.y_calle = y_calle;
+	}
+	public String getAsunto() {
+		return asunto;
+	}
+	public void setAsunto(String asunto) {
+		this.asunto = asunto;
+	}
+	public String getPunto_referencia() {
+		return punto_referencia;
+	}
+	public void setPunto_referencia(String punto_referencia) {
+		this.punto_referencia = punto_referencia;
+	}
+	public String getId_sentido() {
+		return id_sentido;
+	}
+	public void setId_sentido(String id_sentido) {
+		this.id_sentido = id_sentido;
+	}
+	public String getFecha_ingreso() {
+		return fecha_ingreso;
+	}
+	public void setFecha_ingreso(String fecha_ingreso) {
+		this.fecha_ingreso = fecha_ingreso;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getId_prioridad() {
+		return id_prioridad;
+	}
+	public void setId_prioridad(String id_prioridad) {
+		this.id_prioridad = id_prioridad;
+	}
+	public String getEntre_calle() {
+		return entre_calle;
+	}
+	public void setEntre_calle(String entre_calle) {
+		this.entre_calle = entre_calle;
+	}
+	public String getId_tramo() {
+		return id_tramo;
+	}
+	public void setId_tramo(String id_tramo) {
+		this.id_tramo = id_tramo;
+	}
+	public String getId_etapa() {
+		return id_etapa;
+	}
+	public void setId_etapa(String id_etapa) {
+		this.id_etapa = id_etapa;
+	}
+	public String getId_area() {
+		return id_area;
+	}
+	public void setId_area(String id_area) {
+		this.id_area = id_area;
+	}
+	public String getId_lugar() {
+		return id_lugar;
+	}
+	public void setId_lugar(String id_lugar) {
+		this.id_lugar = id_lugar;
+	}
+	public String getLatitud() {
+		return latitud;
+	}
+	public void setLatitud(String latitud) {
+		this.latitud = latitud;
+	}
+	public String getLongitud() {
+		return longitud;
+	}
+	public void setLongitud(String longitud) {
+		this.longitud = longitud;
+	}
+	public String getId_motivo() {
+		return id_motivo;
+	}
+	public void setId_motivo(String id_motivo) {
+		this.id_motivo = id_motivo;
+	}
+	public String getId_tipo() {
+		return id_tipo;
+	}
+	public void setId_tipo(String id_tipo) {
+		this.id_tipo = id_tipo;
+	}
+	public String getId_via() {
+		return id_via;
+	}
+	public void setId_via(String id_via) {
+		this.id_via = id_via;
+	}
+	public String getId_ciudadano() {
+		return id_ciudadano;
+	}
+	public void setId_ciudadano(String id_ciudadano) {
+		this.id_ciudadano = id_ciudadano;
+	}
+	public String getId_ticket() {
+		return id_ticket;
+	}
+	public void setId_ticket(String id_ticket) {
+		this.id_ticket = id_ticket;
+	}
+	
+}
---
+++ src/cmdf2011/weff/rest/TicketRest.java
@@ -0,0 +1,70 @@
+package cmdf2011.weff.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cmdf2011.weff.beans.Ticket;
+import cmdf2011.weff.beans.Via;
+
+import android.util.Log;
+
+public class TicketRest extends GetJson {
+
+	protected static final String TAG = "TcketsRest";
+	
+	/**
+	 * 
+	 * 
+	 * @return
+	 */
+	public static List<Ticket> findTiketsWhereIdCiudadanoEquals(Integer idCiudadano, Integer limit){
+		List<Ticket> list = new ArrayList<Ticket>();
+        final String methodUrl = "Tickets3/findTiketsWhereIdCiudadanoEquals?x-presto-resultFormat=json&limit="+limit+"&idCiudadano="+idCiudadano+"&x-p-anonymous=true";
+        
+        //TODO replace with presto
+        //String result = queryRESTurl(baseUrl + methodUrl);
+        String result = "{\"records\":{\"record\":[{\"y_calle\":\"bvnbv\",\"asunto\":\"bache\",\"punto_referencia\":\"asdsa\",\"id_sentido\":\"1\",\"fecha_ingreso\":\"2011-10-20 17:43:15.773\",\"descripcion\":\"6666 seis again six\",\"id_prioridad\":\"1\",\"entre_calle\":\"gsdfsdf\",\"foto_inicial\":\"\",\"id_tramo\":\"1\",\"id_etapa\":\"1\",\"id_area\":\"1\",\"id_lugar\":\"1\",\"latitud\":\"666.0\",\"longitud\":\"666666.0\",\"id_motivo\":\"1\",\"id_tipo\":\"1\",\"id_via\":\"1\",\"id_ciudadano\":\"1\",\"id_ticket\":\"35\"},{\"y_calle\":\"bvnbv\",\"asunto\":\"bache\",\"punto_referencia\":\"asdsa\",\"id_sentido\":\"1\",\"fecha_ingreso\":\"2011-10-20 17:43:29.413\",\"descripcion\":\"6666 seis again six\",\"id_prioridad\":\"1\",\"entre_calle\":\"gsdfsdf\",\"foto_inicial\":\"\",\"id_tramo\":\"1\",\"id_etapa\":\"1\",\"id_area\":\"1\",\"id_lugar\":\"1\",\"latitud\":\"666.0\",\"longitud\":\"666666.0\",\"id_motivo\":\"1\",\"id_tipo\":\"1\",\"id_via\":\"1\",\"id_ciudadano\":\"1\",\"id_ticket\":\"36\"},{\"y_calle\":\"bvnbv\",\"asunto\":\"bache\",\"punto_referencia\":\"asdsa\",\"id_sentido\":\"1\",\"fecha_ingreso\":\"2011-10-20 17:43:34.917\",\"descripcion\":\"6666 seis again six\",\"id_prioridad\":\"1\",\"entre_calle\":\"gsdfsdf\",\"foto_inicial\":\"\",\"id_tramo\":\"1\",\"id_etapa\":\"1\",\"id_area\":\"1\",\"id_lugar\":\"1\",\"latitud\":\"666.0\",\"longitud\":\"666666.0\",\"id_motivo\":\"1\",\"id_tipo\":\"1\",\"id_via\":\"1\",\"id_ciudadano\":\"1\",\"id_ticket\":\"37\"},{\"y_calle\":\"bvnbv\",\"asunto\":\"bache\",\"punto_referencia\":\"asdsa\",\"id_sentido\":\"1\",\"fecha_ingreso\":\"2011-10-20 17:49:46.807\",\"descripcion\":\"\",\"id_prioridad\":\"1\",\"entre_calle\":\"gsdfsdf\",\"foto_inicial\":\"\",\"id_tramo\":\"1\",\"id_etapa\":\"1\",\"id_area\":\"1\",\"id_lugar\":\"1\",\"latitud\":\"0.0\",\"longitud\":\"0.0\",\"id_motivo\":\"1\",\"id_tipo\":\"1\",\"id_via\":\"1\",\"id_ciudadano\":\"1\",\"id_ticket\":\"38\"},{\"y_calle\":\"bvnbv\",\"asunto\":\"bache\",\"punto_referencia\":\"asdsa\",\"id_sentido\":\"1\",\"fecha_ingreso\":\"2011-10-20 17:49:46.977\",\"descripcion\":\"\",\"id_prioridad\":\"1\",\"entre_calle\":\"gsdfsdf\",\"foto_inicial\":\"\",\"id_tramo\":\"1\",\"id_etapa\":\"1\",\"id_area\":\"1\",\"id_lugar\":\"1\",\"latitud\":\"0.0\",\"longitud\":\"0.0\",\"id_motivo\":\"1\",\"id_tipo\":\"1\",\"id_via\":\"1\",\"id_ciudadano\":\"1\",\"id_ticket\":\"39\"},{\"y_calle\":\"bvnbv\",\"asunto\":\"bache\",\"punto_referencia\":\"asdsa\",\"id_sentido\":\"1\",\"fecha_ingreso\":\"2011-10-20 17:52:26.807\",\"descripcion\":\"6666 seis again six\",\"id_prioridad\":\"1\",\"entre_calle\":\"gsdfsdf\",\"foto_inicial\":\"\",\"id_tramo\":\"1\",\"id_etapa\":\"1\",\"id_area\":\"1\",\"id_lugar\":\"1\",\"latitud\":\"666.0\",\"longitud\":\"666666.0\",\"id_motivo\":\"1\",\"id_tipo\":\"1\",\"id_via\":\"1\",\"id_ciudadano\":\"1\",\"id_ticket\":\"40\"},{\"y_calle\":\"bvnbv\",\"asunto\":\"bache\",\"punto_referencia\":\"asdsa\",\"id_sentido\":\"1\",\"fecha_ingreso\":\"2011-10-20 20:26:08.46\",\"descripcion\":\"9999  99 9 99 9\",\"id_prioridad\":\"1\",\"entre_calle\":\"gsdfsdf\",\"foto_inicial\":\"\",\"id_tramo\":\"1\",\"id_etapa\":\"1\",\"id_area\":\"1\",\"id_lugar\":\"1\",\"latitud\":\"999.0\",\"longitud\":\"999.0\",\"id_motivo\":\"1\",\"id_tipo\":\"1\",\"id_via\":\"1\",\"id_ciudadano\":\"1\",\"id_ticket\":\"41\"},{\"y_calle\":\"bvnbv\",\"asunto\":\"bache\",\"punto_referencia\":\"asdsa\",\"id_sentido\":\"1\",\"fecha_ingreso\":\"2011-10-20 21:03:09.157\",\"descripcion\":\"10 - con foto\",\"id_prioridad\":\"1\",\"entre_calle\":\"gsdfsdf\",\"foto_inicial\":\"\",\"id_tramo\":\"1\",\"id_etapa\":\"1\",\"id_area\":\"1\",\"id_lugar\":\"1\",\"latitud\":\"10.0\",\"longitud\":\"10.0\",\"id_motivo\":\"1\",\"id_tipo\":\"1\",\"id_via\":\"1\",\"id_ciudadano\":\"1\",\"id_ticket\":\"46\"}]}}";
+        
+        try{
+        		JSONObject json = new JSONObject(result);
+        		JSONArray jsonArray = json.getJSONObject("records").getJSONArray("record");
+        		Log.i(TAG, result);
+        		
+        		for (int i = 0; i < jsonArray.length(); i++) {
+        			JSONObject pJson = jsonArray.getJSONObject(i);
+        			Ticket ticket = new Ticket();
+        			ticket.setAsunto(pJson.getString("asunto"));
+        			ticket.setDescripcion(pJson.getString("descripcion"));
+        			ticket.setEntre_calle(pJson.getString("entre_calle"));
+        			ticket.setFecha_ingreso(pJson.getString("fecha_ingreso"));
+        			ticket.setId_area(pJson.getString("id_area"));
+        			ticket.setId_ciudadano(pJson.getString("id_ciudadano"));
+        			ticket.setId_etapa(pJson.getString("id_etapa"));
+        			ticket.setId_lugar(pJson.getString("id_lugar"));
+        			ticket.setId_motivo(pJson.getString("id_motivo"));
+        			ticket.setId_prioridad(pJson.getString("id_prioridad"));
+        			ticket.setId_sentido(pJson.getString("id_sentido"));
+        			ticket.setId_ticket(pJson.getString("id_ticket"));
+        			ticket.setId_tipo(pJson.getString("id_tipo"));
+        			ticket.setId_tramo(pJson.getString("id_tramo"));
+        			ticket.setId_via(pJson.getString("id_via"));
+        			ticket.setLatitud(pJson.getString("latitud"));
+        			ticket.setLongitud(pJson.getString("longitud"));
+        			ticket.setPunto_referencia(pJson.getString("punto_referencia"));
+        			ticket.setY_calle(pJson.getString("y_calle"));
+        			
+        			list.add(ticket);
+        		}
+        }
+        catch (JSONException e) {
+                Log.e("JSON", "There was an error parsing the JSON", e);
+        }
+        
+		return list;
+	}
+	
+}
